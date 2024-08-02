64cb53f55cba3c006ce1733ec8da7a6a8fdd3051
Nikesh Borkar <99666800+NikeshBorkar@users.noreply.github.com>
2024-08-02 23:55:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+document.getElementById('change_button').addEventListener('click', changeColor);
+document.getElementById('reset').addEventListener('click', resetColors);
+
+function changeColor() {
+    const blockId = document.getElementById('block_id').value;
+    const color = document.getElementById('colour_id').value;
+    resetColors();
+    const block = document.getElementById(blockId);
+    if (block) {
+        block.style.backgroundColor = color;
+    }
+	document.getElementById('block_id').value = "";
+    document.getElementById('colour_id').value = "";
+}
+
+function resetColors() {
+    const gridItems = document.querySelectorAll('.grid-item');
+    gridItems.forEach(item => {
+        item.style.backgroundColor = 'transparent';
+    });
+}
